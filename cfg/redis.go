4921086f239b097c50a2f01f0867fc003dab7c8e
Suwandre <suwandresukijat@gmail.com>
2025-02-27 12:23:03 +0800

    rf: reverted redis connection

--- cfg/redis.go
+++ cfg/redis.go
@@ -13,9 +13,8 @@ var RDB *redis.Client
 // Initializes a connection to Redis.
 func InitRedis() {
 	RDB = redis.NewClient(&redis.Options{
-		Addr:     "redis.railway.internal:6379",
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
+		Addr: os.Getenv("REDIS_URL"),
+		DB:   0,
 	})
 
 	_, err := RDB.Ping(context.Background()).Result()
