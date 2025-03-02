d109ad1b92acb6121702dfc72372a258c28cf692
Suwandre <suwandresukijat@gmail.com>
2025-03-02 18:24:51 +0700

    chore: env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
