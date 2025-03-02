e809b821c6134b069b511657e18e06ec6ae9e03b
Suwandre <suwandresukijat@gmail.com>
2025-03-02 20:01:47 +0700

    rf: load dotenv earlier

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
