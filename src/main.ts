7139ef151bfc4fcf6bc265f34f7c7996446aeb74
Suwandre <suwandresukijat@gmail.com>
2025-03-02 18:20:43 +0700

    chore: eslint LF; added fastify to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: '*',
+  });
+
+  // Start Fastify server
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 }
 bootstrap();
