729dd3b30d4e0e7236dce35eb17a45b101b2357c
Suwandre <suwandresukijat@gmail.com>
2025-03-06 23:28:43 +0700

    rf: import drill service for drilling cycle module

--- src/drills/drilling-cycle.module.ts
+++ src/drills/drilling-cycle.module.ts
@@ -15,6 +15,7 @@ import { DrillModule } from './drill.module';
 import { PoolOperatorModule } from 'src/pools/pool-operator.module';
 import { PoolModule } from 'src/pools/pool.module';
 import { RedisModule } from 'src/common/redis.module';
+import { DrillService } from './drill.service';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { RedisModule } from 'src/common/redis.module';
     DrillingSessionModule, // Import DrillingSessionModule
     OperatorModule, // Import OperatorModule
     DrillModule, // Import DrillModule
+    DrillService, // Import DrillService
     PoolOperatorModule, // Import PoolOperatorModule
     PoolModule, // Import PoolModule
     RedisModule, // Import RedisModule
