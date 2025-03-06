a85a8f04bc18220dfe954f2c0bdb335113dc5e1a
Suwandre <suwandresukijat@gmail.com>
2025-03-06 11:56:43 +0700

    Rf: import pool module in pool operator module

--- src/pools/pool-operator.module.ts
+++ src/pools/pool-operator.module.ts
@@ -5,12 +5,14 @@ import {
   PoolOperator,
   PoolOperatorSchema,
 } from './schemas/pool-operator.schema';
+import { PoolModule } from './pool.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PoolOperator.name, schema: PoolOperatorSchema },
     ]), // Register Pool schema
+    PoolModule,
   ],
   controllers: [], // Expose API endpoints
   providers: [PoolOperatorService], // Business logic for pool operators
