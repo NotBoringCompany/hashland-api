4b3587d8d07dbce7b9db88aa34914d05a8e366ce
Dwa Meizadewa <infamous0192@gmail.com>
2025-03-06 14:31:29 +0800

    rf: fixed jwt verify

--- src/auth/jwt-auth.controller.ts
+++ src/auth/jwt-auth.controller.ts
@@ -1,5 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from './jwt/jwt-auth.guard';
+import { ApiResponse as AppApiResponse } from 'src/common/dto/response.dto';
+import { Operator } from 'src/operators/schemas/operator.schema';
+import { OperatorService } from 'src/operators/operator.service';
 
 /**
  * Controller for JWT authentication operations
@@ -7,32 +16,34 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('JWT Authentication')
 @Controller('auth/jwt')
 export class JwtAuthController {
+  constructor(private readonly operatorService: OperatorService) {}
+
   /**
    * Verifies a JWT token and returns the operator information
    * @param req - The request object containing the authenticated user
    * @returns ApiResponse containing the operator information
    */
-  // @ApiOperation({
-  //   summary: 'Verify JWT token',
-  //   description: 'Verifies the JWT token and returns the operator information',
-  // })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Token is valid',
-  // })
-  // @ApiResponse({
-  //   status: 401,
-  //   description: 'Unauthorized - Invalid or expired token',
-  // })
-  // @ApiBearerAuth()
-  // @UseGuards(JwtAuthGuard)
-  // @Get('verify')
-  // async verifyToken(@Request() req): Promise<AppApiResponse<Operator>> {
-  //   const operator = await this.operatorService.findById(req.user.userId);
-  //   return new AppApiResponse<Operator>(
-  //     200,
-  //     'Token verified successfully',
-  //     operator,
-  //   );
-  // }
+  @ApiOperation({
+    summary: 'Verify JWT token',
+    description: 'Verifies the JWT token and returns the operator information',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Token is valid',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - Invalid or expired token',
+  })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('verify')
+  async verifyToken(@Request() req): Promise<AppApiResponse<Operator>> {
+    const operator = await this.operatorService.findById(req.user.userId);
+    return new AppApiResponse<Operator>(
+      200,
+      'Token verified successfully',
+      operator,
+    );
+  }
 }
