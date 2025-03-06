70e4d10b25e694467820a22ef4ea4e7b56c27ac3
Suwandre <suwandresukijat@gmail.com>
2025-03-06 13:11:26 +0700

    rf: pool operator module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,13 +10,13 @@ import { Operator, OperatorSchema } from '../operators/schemas/operator.schema';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { OperatorModule } from 'src/operators/operator.module';
 import { OperatorService } from 'src/operators/operator.service';
-import { PoolOperatorService } from 'src/pools/pool-operator.service';
+import { PoolOperatorModule } from 'src/pools/pool-operator.module';
 
 @Module({
   imports: [
     ConfigModule,
     OperatorModule,
-    PoolOperatorService,
+    PoolOperatorModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
