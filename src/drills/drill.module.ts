5051f642c50dda4757a02449b6f256bc3c4384ca
Suwandre <suwandresukijat@gmail.com>
2025-03-06 23:33:29 +0700

    rf: drill module providers and exports now includes DrillService

--- src/drills/drill.module.ts
+++ src/drills/drill.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Drill, DrillSchema } from './schemas/drill.schema';
+import { DrillService } from './drill.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Drill.name, schema: DrillSchema }]),
   ],
+  providers: [DrillService],
   exports: [
     MongooseModule.forFeature([{ name: Drill.name, schema: DrillSchema }]),
+    DrillService,
   ],
 })
 export class DrillModule {}
