8d9244f246f2f5e13314ac1848aa106ae5f2f33a
Suwandre <suwandresukijat@gmail.com>
2025-03-10 12:10:44 +0700

    rf: updated equityToActualEff values

--- src/drills/drill.service.ts
+++ src/drills/drill.service.ts
@@ -17,10 +17,10 @@ export class DrillService {
    * Fetches the `actualEff` value for basic drills given an operator's weighted asset equity.
    */
   equityToActualEff(equity: number): number {
-    const A = 4500; // Controls early exponential boost.
-    const B = 0.000015; // Controls how fast the exponential growth slows down.
-    const C = 20000; // Controls the logarithmic scaling.
-    const D = 0.00007; // Controls logarithmic growth rate.
+    const A = 22500; // Controls early exponential boost.
+    const B = 0.00001; // Controls how fast the exponential growth slows down.
+    const C = 42500; // Controls the logarithmic scaling.
+    const D = 0.0002; // Controls logarithmic growth rate.
 
     return 500 + A * (1 - Math.exp(-B * equity)) + C * Math.log(1 + D * equity);
   }
