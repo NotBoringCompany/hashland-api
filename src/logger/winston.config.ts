336256bda5968ebab9498f1b214e188aca0e022d
Suwandre <suwandresukijat@gmail.com>
2025-04-18 00:49:24 +0800

    rf: converted back to .log for winston

--- src/logger/winston.config.ts
+++ src/logger/winston.config.ts
@@ -3,7 +3,6 @@ import * as winston from 'winston';
 
 export const winstonConfig: winston.LoggerOptions = {
   transports: [
-    // Keep console output in pretty Nest format
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
@@ -12,15 +11,9 @@ export const winstonConfig: winston.LoggerOptions = {
         }),
       ),
     }),
-
-    // Update file output to be structured JSON
     new winston.transports.File({
       filename: 'logs/perf.log',
       level: 'info',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(), // <- key change here
-      ),
     }),
   ],
 };
