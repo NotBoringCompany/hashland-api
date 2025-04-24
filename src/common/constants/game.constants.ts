4f0957c60c79bf6e5409cccd11229033da4b909e
Suwandre <suwandresukijat@gmail.com>
2025-04-24 18:46:49 +0800

    rf: cycles disabled by default again

--- src/common/constants/game.constants.ts
+++ src/common/constants/game.constants.ts
@@ -66,7 +66,7 @@ export const GAME_CONSTANTS = {
      *
      * If this is `false`, no new drilling cycles will be created.
      */
-    ENABLED: true,
+    ENABLED: false,
     /**
      * The total aimed issuance of $HASH until `TOTAL_CYCLES` are reached.
      */
